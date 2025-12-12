6b0e538e0083ab9c31410b85842a5341a4c6d64e
Majidul17068 <majidulislam17068@gmail.com>
2025-12-12 17:18:26 +0600

    refactorV2

--- js/app.js
+++ js/app.js
@@ -1,70 +1,51 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 60,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#9F9FA9"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+        "color": "#9F9FA9"
       }
     },
     "opacity": {
-      "value": 0.5,
-      "random": false,
+      "value": 0.6,
+      "random": true,
       "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
+        "enable": false
       }
     },
     "size": {
       "value": 3,
       "random": true,
       "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
+        "enable": false
       }
     },
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
+      "color": "#9F9FA9",
+      "opacity": 0.5,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 2,
       "direction": "none",
       "random": false,
       "straight": false,
       "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
+      "bounce": false
     }
   },
   "interactivity": {
@@ -82,29 +63,15 @@ particlesJS("particles-js", {
     },
     "modes": {
       "grab": {
-        "distance": 150,
+        "distance": 140,
         "line_linked": {
           "opacity": 1
         }
       },
-      "bubble": {
-        "distance": 250,
-        "size": 0,
-        "duration": 2,
-        "opacity": 0,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
       "push": {
         "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
       }
     }
   },
   "retina_detect": true
-}); 
+});
